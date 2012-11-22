23e08b42444b8cb67416b497ca8d9d5725e02335
Alexander Svyazin <guybrush@live.ru>
2012-11-22 22:51:47 +0600

    PROB problem

---
+++ prob.hs
@@ -0,0 +1,16 @@
+square :: Float -> Float
+square x = x * x
+
+prob1 :: Float -> Float
+prob1 x = 0.5 * ((square x) + (square (1 - x)))
+
+prob = map prob1
+
+readFloat :: String -> Float
+readFloat = read
+
+parseInput :: String -> [Float]
+parseInput str = map readFloat $ words str
+
+answer :: String -> String
+answer = unwords . map show . prob . parseInput
